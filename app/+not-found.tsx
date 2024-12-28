fa72ecc28a6a521f64a3d82efc03f7aa8a450477
Running water <alsgks1218@naver.com>
2024-12-28 21:47:18 +0900

    tabs와 screen 추가

---
+++ app/+not-found.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { View, StyleSheet } from "react-native";
+import { Link, Stack } from "expo-router";
+
+export default function NotFoundScreen() {
+  return (
+    <>
+      <Stack.Screen options={{ title: "Oops! Not found!" }} />
+      <View style={styles.container}>
+        <Link style={styles.button} href="/">
+          Go back to home
+        </Link>
+      </View>
+    </>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#25292e",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  button: {
+    fontSize: 20,
+    textDecorationLine: "underline",
+    color: "#fff",
+  },
+});
